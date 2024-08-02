d59e270922ecc34696d00b6f4f3927363f60b6e9
juanpa1233238055 <amerikmv@gmail.com>
2024-08-01 19:00:12 -0500

    Add files via upload

---
+++ C23-AM-v4-main/sketch.js
@@ -0,0 +1,21 @@
+const Engine = Matter .Engine;
+const worl = Matter .world
+const Bodies =  Matter .Bodies;
+const Body = mtter .Body
+
+var enegine, world;
+function setup() {
+    createCanvas(400,400);
+enegine = Engine.create
+ world = enegine.world;
+ var ball_option = {
+    restitution;}:
+ }
+}
+
+function draw() 
+{
+  background 
+
+}
+
